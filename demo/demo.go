6c975cf8a6662ea1ac920fa49f361c6d79849e50
Ox Cart <ox.to.a.cart@gmail.com>
2020-04-30 11:12:20 -0500

    Added demo program

---
+++ demo/demo.go
@@ -0,0 +1,20 @@
+// this is a simple demo program that runs whatever command you give it with elevated privileges,
+// like
+//   ./demo ls /tmp
+//
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/getlantern/elevate"
+)
+
+func main() {
+	out, err := elevate.Command(os.Args[1], os.Args[2:]...).CombinedOutput()
+	fmt.Println(string(out))
+	if err != nil {
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/getlantern/elevate
+
+go 1.13
+
+require (
+	github.com/getlantern/byteexec v0.0.0-20170405023437-4cfb26ec74f4
+	github.com/getlantern/filepersist v0.0.0-20160317154340-c5f0cd24e799 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+)
